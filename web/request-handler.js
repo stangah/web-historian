47b036cdb7abb8582a7b7dbc638d9dbfaaa68112
Patrick <pstanger@gmail.com>
2013-10-16 20:36:33 -0700

    Can now successfully add sites to sites.txt and download the site HTML

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var caching = require('../workers/htmlfetcher');
+var htmlfetchers = require('../workers/lib/html-fetcher-helpers');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
 var headers = {
@@ -54,14 +55,14 @@ module.exports.handleRequest = function (req, res) {
       fs.exists(path.resolve(__dirname, lookup), function(exists) {
         if(exists) {
           fs.readFile(path.resolve(__dirname, lookup), function(err, data) {
-          if (err) {
-            console.log(err);
-            res.writeHead(500);
-            res.end();
-          } else {
-            res.writeHead(200, {"Content-Type": contentType });
-            res.end(data);
-          }
+            if (err) {
+              console.log(err);
+              res.writeHead(500);
+              res.end();
+            } else {
+              res.writeHead(200, {"Content-Type": contentType });
+              res.end(data);
+            }
           });
         } else {
           res.writeHead(404, headers);
@@ -70,6 +71,7 @@ module.exports.handleRequest = function (req, res) {
       });
       break;
     case 'POST':
+      console.log('posting');
       // the problem is that we are not writing to the test sites.txt
       res.writeHead(302, headers);
       collectData(req, function(data){
@@ -78,11 +80,14 @@ module.exports.handleRequest = function (req, res) {
         fs.appendFile(module.exports.datadir, url + '\n', function (err) {
         if (err) throw err;
           console.log('Data successfully appended to file!');
+          htmlfetchers.downloadUrls([url,'']);
         });
       });
       res.end();
       break;
     case 'OPTIONS':
+      res.writeHead(200, headers);
+      res.end();
       break;
     default:
       break;
